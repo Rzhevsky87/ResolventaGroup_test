f2fb2cd0d9b9a51d083b6f7993149ab8f0ce6a55
Rzhevsky87 <egorbarisovi4@gmail.com>
2021-03-31 12:43:51 +0500

    init

---
+++ resources/views/components/request-for-quotation.blade.php
@@ -0,0 +1,13 @@
+<x-main.layout>
+    <x-slot name="title">
+        Custom Title
+    </x-slot>
+
+    <div>
+        <a
+            href="https://api.exchangeratesapi.io/history?start_at=2021-03-30&end_at=2021-03-30&symbols=USD,RUB"
+        >
+            Катировка доллара к рублю
+        </a>
+    </div>
+</x-main-layout>
