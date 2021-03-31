eae9f391978a42bec0e30457be3a9951770e6610
Rzhevsky87 <egorbarisovi4@gmail.com>
2021-04-01 00:10:28 +0500

    front added br

--- resources/views/components/request-for-quotation.blade.php
+++ resources/views/components/request-for-quotation.blade.php
@@ -5,22 +5,25 @@
 
     <div>
         <h3>
-            Выберите из какой валюты в какую и на какую дату нужно перевести
+            Выберите из какой валюты в какую и за какой период нужно конвертировать
         </h3>
         <form method="GET" action="{{ route('getRate') }}">
             @csrf
             <div>
                 <label for="from">Из</label>
+                <br>
                 <select size="3" multiple id="from" name="from">
                     <option disabled>Выберите валюту</option>
-                    <option value="RUB">Российский рубль</option>
+                    <option value="RUB">RUB</option>
                     <option selected value="USD">USD</option>
                     <option value="EUR">EUR</option>
                     <option value="JPY">JPY</option>
                 </select>
             </div>
+            <br>
             <div>
                 <label for="to">В</label>
+                <br>
                 <select size="3" multiple id="to" name="to">
                     <option disabled>Выберите валюту</option>
                     <option selected value="RUB">RUB</option>
@@ -29,15 +32,17 @@
                     <option value="JPY">JPY</option>
                 </select>
             </div>
+            <br>
             <div>
                 <label for="start">С</label>
                 <input type="date" id="start" name="start" required>
             </div>
+            <br>
             <div>
                 <label for="end">По</label>
                 <input type="date" id="end" name="end" required>
             </div>
-
+            <br><br>
             <button type="submit" class="">Запросить</button>
         </form>
     </div>
